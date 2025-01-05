a722e7461ba65599073ca2d530e56f9aff85cded
erick-augusto <erick_aug@hotmail.com>
2025-01-05 17:51:35 -0300

    projeto final do curso

---
+++ src/main/java/br/com/alura/screenmatch_frases/service/FraseService.java
@@ -0,0 +1,19 @@
+package br.com.alura.screenmatch_frases.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.screenmatch_frases.dto.FraseDTO;
+import br.com.alura.screenmatch_frases.model.Frase;
+import br.com.alura.screenmatch_frases.repository.FraseRepository;
+
+@Service
+public class FraseService {
+    @Autowired
+    private FraseRepository repositorio;
+
+    public FraseDTO obterFraseAleatoria() {
+        Frase frase = repositorio.buscaFraseAleatoria();
+        return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
+    }
+}
